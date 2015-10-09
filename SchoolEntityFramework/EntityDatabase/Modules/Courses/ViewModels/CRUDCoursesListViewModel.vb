fa4be7d8dc988b0ba569db047cdd05f8b71f466f
Javier Briones <rodrigojsbrio@gmail.com>
2015-10-09 13:31:00 -0600

    CRUDs and Views improvements

--- SchoolEntityFramework/EntityDatabase/Modules/Courses/ViewModels/CRUDCoursesListViewModel.vb
+++ SchoolEntityFramework/EntityDatabase/Modules/Courses/ViewModels/CRUDCoursesListViewModel.vb
@@ -6,19 +6,32 @@ Imports BusinessLogic.Services.Interfaces
 
 Namespace Modules.Courses.ViewModels
     Public Class CRUDCoursesListViewModel
-
-
         Inherits ViewModelBase
-
-        Private _course As New Course
-        Public _okCommand As ICommand
-        Public _cancelCommand As ICommand
         Public _window As CRUDCoursesListView
+        Private _course As New Course
         Private _departments As ObservableCollection(Of Department)
-        Private _selectedDepartment As Department
+        Private _showD As Department
         Private dataAccess As IDepartmentService
+        Public _accept As ICommand
+        Public _cancel As ICommand
 
+        Public ReadOnly Property Accept As ICommand
+            Get
+                If Me._accept Is Nothing Then
+                    Me._accept = New RelayCommand(AddressOf Aprove)
+                End If
+                Return Me._accept
+            End Get
+        End Property
 
+        Public ReadOnly Property Cancel As ICommand
+            Get
+                If Me._cancel Is Nothing Then
+                    Me._cancel = New RelayCommand(AddressOf Deny)
+                End If
+                Return Me._cancel
+            End Get
+        End Property
         Public Property Title As String
             Get
                 Return Me._course.Title
@@ -39,13 +52,13 @@ Namespace Modules.Courses.ViewModels
             End Set
         End Property
 
-        Public Property SelectedDepartment As Department
+        Public Property ShowD As Department
             Get
-                Return _selectedDepartment
+                Return _showD
             End Get
             Set(value As Department)
                 _course.Department = value
-                OnPropertyChanged("SelectedDepartment")
+                OnPropertyChanged("ShowD")
             End Set
         End Property
 
@@ -58,40 +71,21 @@ Namespace Modules.Courses.ViewModels
                 OnPropertyChanged("Departments")
             End Set
         End Property
-
-        Public ReadOnly Property OkButton As ICommand
-            Get
-                If Me._okCommand Is Nothing Then
-                    Me._okCommand = New RelayCommand(AddressOf OkCommand)
-                End If
-                Return Me._okCommand
-            End Get
-        End Property
-
-        Public ReadOnly Property CancelButton As ICommand
-            Get
-                If Me._cancelCommand Is Nothing Then
-                    Me._cancelCommand = New RelayCommand(AddressOf CancelCommand)
-                End If
-                Return Me._cancelCommand
-            End Get
-        End Property
-
-        Sub OkCommand()
+        Sub Aprove()
             Try
                 Dim courses As IQueryable(Of Course) = DataContext.DBEntities.Courses
-                For Each element In Courses
+                For Each element In courses
                     _course.CourseID = Integer.Parse(element.CourseID.ToString) + 1
                 Next
                 DataContext.DBEntities.Courses.Add(_course)
                 DataContext.DBEntities.SaveChanges()
                 _window.Close()
             Catch ex As Exception
-                MessageBox.Show("No se ha podido ingresar el curso", MsgBoxStyle.Critical)
+                MessageBox.Show("Error, no se ha ingresado el curso", MsgBoxStyle.Critical)
             End Try
         End Sub
 
-        Sub CancelCommand()
+        Sub Deny()
             _window.Close()
         End Sub
 
--- SchoolEntityFramework/EntityDatabase/Modules/Courses/ViewModels/CoursesViewModel.vb
+++ SchoolEntityFramework/EntityDatabase/Modules/Courses/ViewModels/CoursesViewModel.vb
@@ -10,11 +10,51 @@ Namespace Modules.Courses.ViewModels
         Inherits ViewModelBase
 
         Public Shadows _course As CRUDCoursesListView
+        Private data As ICourseService
+        Private _show As Course
+        Private _courses As ObservableCollection(Of Course)
         Private _delete As ICommand
         Private _insert As ICommand
-        Private _selected As Course
-        Private _courses As ObservableCollection(Of Course)
-        Private dataAccess As ICourseService
+
+        Public Property Add As ICommand
+            Get
+                If Me._insert Is Nothing Then
+                    Me._insert = New RelayCommand(AddressOf DatabaseDepartment)
+                End If
+                Return Me._insert
+            End Get
+            Set(value As ICommand)
+                _insert = value
+            End Set
+        End Property
+        Sub DatabaseDepartment()
+            Using school As New SchoolEntities
+                _course = New CRUDCoursesListView
+                _course.ShowDialog()
+                Charge()
+            End Using
+        End Sub
+        Public Property Delete As ICommand
+            Get
+                If _delete Is Nothing Then
+                    _delete = New RelayCommand(AddressOf DatabaseDelete)
+                End If
+                Return _delete
+            End Get
+            Set(value As ICommand)
+                _delete = value
+            End Set
+        End Property
+
+        Sub DatabaseDelete()
+            If Show IsNot Nothing Then
+                DataContext.DBEntities.Courses.Remove((From item In DataContext.DBEntities.Courses
+                                 Where Show.CourseID = item.CourseID
+                                 Select item).FirstOrDefault)
+                DataContext.DBEntities.SaveChanges()
+                Charge()
+            End If
+        End Sub
 
         Public Property Courses As ObservableCollection(Of Course)
             Get
@@ -26,79 +66,33 @@ Namespace Modules.Courses.ViewModels
             End Set
         End Property
 
-        ' Function to get all departments from service
-        Private Function GetAllCourses() As IQueryable(Of Course)
-            Return Me.dataAccess.GetAllCourses
+        Private Function GetCourses() As IQueryable(Of Course)
+            Return Me.data.GetAllCourses
         End Function
+        
 
-        Public Property DeleteCommand As ICommand
+        Public Property Show As Course
             Get
-                If _delete Is Nothing Then
-                    _delete = New RelayCommand(AddressOf DeleteDB)
-                End If
-                Return _delete
-            End Get
-            Set(value As ICommand)
-                _delete = value
-            End Set
-        End Property
-
-        Sub DeleteDB()
-            If Selected IsNot Nothing Then
-                DataContext.DBEntities.Courses.Remove((From item In DataContext.DBEntities.Courses
-                                 Where Selected.CourseID = item.CourseID
-                                 Select item).FirstOrDefault)
-                DataContext.DBEntities.SaveChanges()
-                Refresh()
-            End If
-        End Sub
-
-        Public Property Selected As Course
-            Get
-                Return _selected
+                Return _show
             End Get
             Set(value As Course)
-                _selected = value
+                _show = value
             End Set
         End Property
 
-        Public Property AddCommand As ICommand
-            Get
-                If Me._insert Is Nothing Then
-                    Me._insert = New RelayCommand(AddressOf AddDepartmentToDB)
-                End If
-                Return Me._insert
-            End Get
-            Set(value As ICommand)
-                _insert = value
-            End Set
-        End Property
-        Sub AddDepartmentToDB()
-            Using school As New SchoolEntities
-                _course = New CRUDCoursesListView
-                _course.ShowDialog()
-                Refresh()
-            End Using
-        End Sub
-
-        Sub Refresh()
-            'Initialize property variable of departments
+        Sub Charge()
             Me.Courses.Clear()
-            ' Register service with ServiceLocator
             ServiceLocator.RegisterService(Of ICourseService)(New CourseService)
-            ' Initialize dataAccess from service
-            Me.dataAccess = GetService(Of ICourseService)()
-            ' Populate departments property variable  
-            For Each element In Me.GetAllCourses
+            Me.data = GetService(Of ICourseService)()
+            For Each element In Me.GetCourses
                 Me._courses.Add(element)
             Next
         End Sub
 
         Sub New()
             Me.Courses = New ObservableCollection(Of Course)
-            Refresh()
+            Charge()
         End Sub
-
     End Class
 End Namespace
 
